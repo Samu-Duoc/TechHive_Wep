43296704db666f55ec739019386c6635e20b0105
Samu-Duoc <sam.fuenzalida@duocuc.cl>
2025-10-22 01:26:27 -0300

    Fix: se agrego e instalo, testing, para pruebas unitarias

--- techhvie-rv/vite.config.ts
+++ techhvie-rv/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+
+ //@ts-ignore
+
+ test: {
+  globals: true,
+  enviorment: 'jsdom',
+  setupFiles: './src/setupTests.ts'
+
+  //setupFiles: './src/test/login.tsx' porque no así?
+ }
+
 })
