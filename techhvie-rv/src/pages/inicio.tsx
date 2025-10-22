92b0588ddafda23aca35c1def61dd6484ac14b40
Samu-Duoc <sam.fuenzalida@duocuc.cl>
2025-10-22 01:46:40 -0300

    Feat: Compoente del incio de la pagina

---
+++ techhvie-rv/src/pages/inicio.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Inicio: React.FC = () => {
+    return (
+        <div className="container mt-5 pt-5">
+            <div className="row">
+                <div className="col-12">
+                    <h1 className="text-center mb-4">Bienvenidos a TechHive</h1>
+                    <p className="lead text-center">
+                        La colmena de la Tecnología 🐝
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Inicio;
