ded277c66fbd0af586f35549618aa4a495cd7ddc
Samu-Duoc <sam.fuenzalida@duocuc.cl>
2025-12-02 13:47:24 -0300

    Acción ingresa todos pueden ver su perfil

--- techhvie-rv2/src/pages/MenuPerfil.tsx
+++ techhvie-rv2/src/pages/MenuPerfil.tsx
@@ -13,10 +13,12 @@ const MenuPerfil: React.FC<Props> = ({ role }) => {
         <ListGroup variant="flush">
             <ListGroup.Item className="menu-title">Mi Cuenta</ListGroup.Item>
 
+            {/* Perfil (disponible para todos los roles) */}
+            <ListGroup.Item as={Link} to="/cuenta/perfil">Perfil</ListGroup.Item>
+
             {/* CLIENTE */}
             {(!role || role === 'CLIENTE') && (
             <>
-                <ListGroup.Item as={Link} to="/cuenta/perfil">Perfil</ListGroup.Item>
                 <ListGroup.Item as={Link} to="/cuenta/mis-ordenes">Mis órdenes</ListGroup.Item>
             </>
             )}
@@ -31,7 +33,8 @@ const MenuPerfil: React.FC<Props> = ({ role }) => {
 
             {/* ADMIN: no mostrar Admin Dashboard aquí; mantener inventario, órdenes y mensajes */}
             {role === 'ADMIN' && (
-            <>
+            <>  
+                
                 <ListGroup.Item as={Link} to="/inventario">Inventario</ListGroup.Item>
                 <ListGroup.Item as={Link} to="/admin/ordenes">Órdenes</ListGroup.Item>
                 <ListGroup.Item as={Link} to="/admin/mensajes">Mensajes</ListGroup.Item>
